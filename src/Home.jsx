e82dc84672f33e16685b4f960561cb209e6bd4a9
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-07-12 13:47:16 +0300

    Adding Home Header

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
 import Navbar from "./components/Navbar";
+import HomeHeader from "./components/HomeHeader";
 
 const Home = function({ navigation }) {
     return(
         <SafeAreaView style={styles.container}>
-            <Text style={styles.text}>Home is here.</Text>
+            <HomeHeader/>
             <Navbar navigation={navigation} active="home"/>
         </SafeAreaView>
     );
@@ -13,7 +14,8 @@ const Home = function({ navigation }) {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: "#E7E7E7"
     },
 
     text: {
