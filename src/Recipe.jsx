4821a87dc221b738fe1a8a7b3584d0e140a7fe62
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-07-19 10:13:02 +0300

    Developing Recipe screen

---
+++ src/Recipe.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import Navbar from "./components/Navbar";
+import RecipeHeader from "./components/RecipeHeader";
+import RecipeView from "./components/RecipeView";
+
+const Recipe = function({ navigation, route }) {
+    const [recipe, setRecipe] = useState(null);
+
+    const getRecipe = function() {
+        fetch("http://192.168.88.16:8000/api/recipe/" + route.params.recipe)
+            .then(response => response.json()
+            .then(res => {
+                if(res.error <= 0)
+                    setRecipe(res.data);
+                else console.log(res);
+            }).catch(error => console.log(error)));
+    };
+
+    useEffect(() => getRecipe(), []);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <RecipeHeader navigation={navigation}/>
+            {recipe != null && <RecipeView recipe={recipe}/>}
+            <Navbar navigation={navigation}/>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#E7E7E7"
+    }
+});
+
+export default Recipe;
