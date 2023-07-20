828fb61015c6edaa86554a6d35950d0cb5afd0f8
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-07-20 17:36:36 +0300

    Adding view for login or profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,12 +1,31 @@
-import React from "react";
-import { Button, SafeAreaView, StyleSheet, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
 import Navbar from "./components/Navbar";
 import ProfileHeader from "./components/ProfileHeader";
+import Login from "./components/Login";
+import ProfileView from "./components/ProfileView";
 
 const Profile = function({ navigation }) {
+    const [user, setUser] = useState(null);
+
+    const isAuth = function() {
+        fetch("http://192.168.88.16:8000/api/user/auth")
+            .then(response => response.json()
+            .then(res => {
+                if(res.error <= 0)
+                    setUser(res.data);
+                else console.log(res);
+            }).catch(error => console.log(error)));
+    };
+
+    useEffect(() => isAuth(), []);
+
     return (
         <SafeAreaView style={styles.container}>
             <ProfileHeader/>
+            {user == null ?
+            <Login setUser={setUser}/> :
+            <ProfileView setUser={setUser}/>}
             <Navbar navigation={navigation} active="profile"/>
         </SafeAreaView>
     );
@@ -15,10 +34,6 @@ const Profile = function({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1
-    },
-
-    text: {
-        color: "blue"
     }
 });
 
