e9f13692040c861d270e2d72cce66f45f235bbf8
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-07-20 14:37:28 +0300

    Developing a header for profile

---
+++ src/components/ProfileHeader.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const ProfileHeader = function() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.titleView}>
+                <Text style={styles.titleText}>Profile</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#5F9F5A",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 50
+    },
+
+    titleView: {},
+
+    titleText: {
+        fontWeight: "bold",
+        fontSize: 25,
+        color: "#FFE16F"
+    }
+});
+
+export default ProfileHeader;
