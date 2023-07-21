5e71e606b6544cd819b436e70bbee2e9554500a3
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-07-21 09:49:04 +0300

    Adding profile picture, username and logout/follow button to Profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -5,16 +5,19 @@ import ProfileHeader from "./components/ProfileHeader";
 import Login from "./components/Login";
 import ProfileView from "./components/ProfileView";
 
-const Profile = function({ navigation }) {
+const Profile = function({ navigation, route }) {
     const [user, setUser] = useState(null);
+    const [profileId, setProfileId] = useState(null);
 
     const isAuth = function() {
         fetch("http://192.168.88.16:8000/api/user/auth")
             .then(response => response.json()
             .then(res => {
-                if(res.error <= 0)
+                if(res.error <= 0) {
                     setUser(res.data);
-                else console.log(res);
+                    if(route.params != undefined)
+                        setProfileId(route.params.profile);
+                } else console.log(res);
             }).catch(error => console.log(error)));
     };
 
@@ -25,7 +28,7 @@ const Profile = function({ navigation }) {
             <ProfileHeader/>
             {user == null ?
             <Login setUser={setUser}/> :
-            <ProfileView setUser={setUser}/>}
+            <ProfileView user={user} setUser={setUser} profileId={profileId}/>}
             <Navbar navigation={navigation} active="profile"/>
         </SafeAreaView>
     );
--- src/components/ProfileView.jsx
+++ src/components/ProfileView.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-import { Button, ScrollView, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Button, Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import TextButton from "./TextButton";
 
-const ProfileView = function({ setUser }) {
+const ProfileView = function({ user, setUser, profileId }) {
+    const [profile, setProfile] = useState(null);
+
     const logout = function() {
         fetch("http://192.168.88.16:8000/api/user/logout")
             .then(response => response.json()
@@ -12,11 +15,112 @@ const ProfileView = function({ setUser }) {
             }).catch(error => console.log(error)));
     };
 
-    return (
-        <ScrollView>
-            <Button title="logout" onPress={logout}/>
-        </ScrollView>
-    );
+    const getProfile = function() {
+        if(profileId == undefined || profileId == null)
+            setProfile({...user});
+        else fetch("http://192.168.88.16:8000/api/profile/" + profileId)
+                .then(response => response.json()
+                .then(res => {
+                    if(res.error <= 0)
+                        setProfile(res.data);
+                    else console.log(res);
+                }).catch(error => console.log(error)));
+    };
+
+    useEffect(() => getProfile(), []);
+
+    if(profile != null)
+        return (
+            <ScrollView style={styles.container}>
+                <View style={styles.informationsView}>
+                    <View style={styles.pictureView}>
+                        {profile.userImage == null ?
+                        <Text style={styles.initialText}>{profile.userName.charAt(0).toUpperCase()}</Text> :
+                        <Image source={{ uri: "http://192.168.88.16:8000/images/user_accounts/" + profile.userImage }}/>}
+                    </View>
+                    <View style={styles.usernameView}>
+                        <Text style={styles.usernameText}>{profile.userName}</Text>
+                        {user.userId == profile.userId ?
+                        <TextButton title="Logout" style={styles.textButton} titleStyle={styles.titleTextButton} pressedStyle={styles.pressedTextButton} onPress={logout}/> :
+                        <TextButton title="Follow" style={styles.textButton} titleStyle={styles.titleTextButton} pressedStyle={styles.pressedTextButton}/>}
+                    </View>
+                </View>
+                
+                {user.userId == profile.userId &&
+                <View style={styles.navView}>
+
+                </View>}
+                
+                <View style={styles.recipesView}>
+
+                </View>
+            </ScrollView>
+        );
+    else return null;
 };
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+
+    informationsView: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 10,
+        paddingVertical: 10
+    },
+
+    pictureView: {
+        width: 80,
+        height: 80,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#5F9F5A",
+        borderRadius: 50
+    },
+
+    initialText: {
+        fontSize: 40,
+        color: "white"
+    },
+
+    usernameView: {
+        paddingHorizontal: 10
+    },
+
+    usernameText: {
+        color: "#696969",
+        fontWeight: "bold",
+        fontSize: 20,
+        marginBottom: 5,
+        width: 100,
+        textAlign: "center"
+    },
+
+    textButton: {
+        backgroundColor: "#5F9F5A",
+        width: 100,
+        paddingVertical: 7,
+        marginBottom: 10
+    },
+
+    titleTextButton: {
+        color: "#FFFFFF"
+    },
+
+    pressedTextButton: {
+        backgroundColor: "#4F824B"
+    },
+
+    navView: {
+
+    },
+
+    recipesView: {
+
+    }
+});
 
 export default ProfileView;
