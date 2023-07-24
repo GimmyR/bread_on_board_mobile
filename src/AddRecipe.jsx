4ccdf978b6c0b4698c477f67b77fd7c399c1d5c8
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-07-24 15:46:41 +0300

    Adding inputs for Recipe Title, Recipe Image, Add Ingredient button, Add Instruction button and Save Recipe button

--- src/AddRecipe.jsx
+++ src/AddRecipe.jsx
@@ -1,12 +1,35 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 import Navbar from "./components/Navbar";
 import AddRecipeHeader from "./components/AddRecipeHeader";
+import TitleInput from "./components/TitleInput";
+import ImageInput from "./components/ImageInput";
+import TextButton from "./components/TextButton";
 
 const AddRecipe = function({ navigation }) {
+    const [recipeTitle, setRecipeTitle] = useState("");
+    const [recipeImage, setRecipeImage] = useState(null);
+
     return (
         <SafeAreaView style={styles.container}>
             <AddRecipeHeader/>
+            <ScrollView style={styles.scrollView}>
+                <View style={styles.titleView}>
+                    <Text style={styles.labelText}>Recipe Title</Text>
+                    <TitleInput value={recipeTitle} onChangeText={setRecipeTitle}/>
+                </View>
+                <View style={styles.imageView}>
+                    <Text style={styles.labelText}>Recipe Image</Text>
+                    <ImageInput value={recipeImage} onChangeText={setRecipeImage} title="browse"/>
+                </View>
+                <View style={styles.addIngredientView}>
+                    <TextButton title="Add ingredient" style={styles.addButton} titleStyle={styles.addButtonTitle}/>
+                </View>
+                <View style={styles.addInstructionView}>
+                    <TextButton title="Add instruction" style={styles.addButton} titleStyle={styles.addButtonTitle}/>
+                </View>
+                <TextButton title="Save recipe" style={styles.saveRecipe} titleStyle={styles.saveRecipeTitle}/>
+            </ScrollView>
             <Navbar navigation={navigation} active="add-recipe"/>
         </SafeAreaView>
     );
@@ -17,9 +40,49 @@ const styles = StyleSheet.create({
         flex: 1
     },
 
-    text: {
-        color: "black"
-    }
+    scrollView: {
+        paddingHorizontal: 10
+    },
+
+    titleView: {
+        marginTop: 5,
+        marginBottom: 10
+    },
+
+    labelText: {
+        color: "#5F9F5A",
+        fontWeight: "bold",
+        marginBottom: 5,
+        marginLeft: 3
+    },
+
+    imageView: {
+        marginBottom: 20
+    },
+
+    addButton: {
+        backgroundColor: "#ADADAD"
+    },
+
+    addButtonTitle: {
+        color: "white"
+    },
+
+    addIngredientView: {
+        marginBottom: 10
+    },
+
+    addInstructionView: {
+        marginBottom: 10
+    },
+
+    saveRecipe: {
+        backgroundColor: "#5F9F5A"
+    },
+
+    saveRecipeTitle: {
+        color: "#FFE16F"
+    },
 });
 
 export default AddRecipe;
