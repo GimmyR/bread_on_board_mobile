82e23701cfb8bfaffc0d97263d8658421fd83446
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-07-28 15:12:21 +0300

    Developing a header for EditRecipe

---
+++ src/components/EditRecipeHeader.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import IconButton from "./IconButton";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
+const EditRecipeHeader = function({ navigation, recipe }) {
+    const icon = { size: 23, color: "white" };
+
+    const goBack = function() {
+        navigation.push("Recipe", { recipe: recipe.recipeId });
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.backView}>
+                <IconButton icon={faArrowLeft} size={icon.size} color={icon.color} onPress={goBack}/>
+            </View>
+            <View style={styles.titleView}>
+                <Text style={styles.titleText}>Edit Recipe</Text>
+            </View>
+            <View style={styles.blankView}></View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#5F9F5A",
+        flexDirection: "row",
+        alignItems: "center",
+        height: 50
+    },
+
+    backView: {
+        flex: 1,
+        marginTop: 2,
+        flexDirection: "row",
+        justifyContent: "center"
+    },
+
+    titleView: {
+        flex: 4,
+        flexDirection: "row",
+        justifyContent: "center"
+    },
+
+    titleText: {
+        fontWeight: "bold",
+        fontSize: 25,
+        color: "#FFE16F"
+    },
+
+    blankView: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center"
+    }
+});
+
+export default EditRecipeHeader;
