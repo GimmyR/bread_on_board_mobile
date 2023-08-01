f2832d8f0599a73083eb40bd0d189b4c2a6b1036
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-08-01 14:40:55 +0300

    Adding Icon Button for adding to favorites

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 import Link from "./Link";
+import IconButton from "./IconButton";
+import { faHeart as faHeartR } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as faHeartS } from "@fortawesome/free-solid-svg-icons";
 
 const RecipeItem = function({ recipe, navigation }) {
+    const [isFavorite, setIsFavorite] = useState(false);
+
     const source = { uri: "http://192.168.88.16:8000/images/recipes/" + recipe.recipeImage };
 
     const onPressRecipe = function() {
@@ -12,6 +17,27 @@ const RecipeItem = function({ recipe, navigation }) {
     const onPressAuthor = function() {
         navigation.push("Profile", { profile: recipe.recipeAuthor.userId });
     };
+
+    const addFavorite = function() {
+        fetch("http://192.168.88.16:8000/api/" + (isFavorite ? "remove-favorite" : "add-favorite") + "/" + recipe.recipeId)
+            .then(response => response.json()
+            .then(res => {
+                if(res.error <= 0)
+                    setIsFavorite(!isFavorite);
+                else console.log(res);
+            }).catch(error => console.log(error)));
+    };
+
+    const checkFavorite = function() {
+        fetch("http://192.168.88.16:8000/api/is-favorite/" + recipe.recipeId)
+            .then(response => response.json()
+            .then(res => {
+                if(res.error <= 0)
+                    setIsFavorite(res.data);
+            }).catch(error => console.log(error)));
+    };
+
+    useEffect(() => checkFavorite(), []);
     
     return (
         <View style={styles.container}>
@@ -22,11 +48,18 @@ const RecipeItem = function({ recipe, navigation }) {
                 <View>
                     <Image source={source} style={styles.recipeImage}/>
                 </View>
+            </Pressable>
+            <View style={styles.recipeFavoriteAuthorView}>
+                <View style={styles.recipeFavoriteView}>
+                    {isFavorite ?
+                    <IconButton icon={faHeartS} color="#5F9F5A" size={20} onPress={addFavorite}/> :
+                    <IconButton icon={faHeartR} color="#000000" size={20} onPress={addFavorite}/>}
+                </View>
                 <View style={styles.recipeAuthorView}>
                     <Text style={styles.recipeByText}>by</Text>
-                    <Link title={recipe.recipeAuthor.userName} style={styles.recipeAuthorText} onPress={onPressAuthor}/>  
+                    <Link title={recipe.recipeAuthor.userName} style={styles.recipeAuthorText} onPress={onPressAuthor}/>
                 </View>
-            </Pressable>
+            </View>
         </View>
     );
 };
@@ -52,7 +85,20 @@ const styles = StyleSheet.create({
         height: 100
     },
 
+    recipeFavoriteAuthorView: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        paddingVertical: 5
+    },
+
+    recipeFavoriteView: {
+        flex: 1,
+        paddingLeft: 10
+    },
+
     recipeAuthorView: {
+        flex: 6,
         paddingHorizontal: 10,
         paddingVertical: 5,
         flexDirection: "row",
