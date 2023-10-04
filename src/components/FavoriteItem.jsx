2fd7c27404041a1bcf9e7bd5ba3de935ab9383f0
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-10-04 10:00:58 +0300

    Display base64 image with FavoriteItem

--- src/components/FavoriteItem.jsx
+++ src/components/FavoriteItem.jsx
@@ -46,7 +46,7 @@ const FavoriteItem = function({ recipe, navigation, refreshFavorites }) {
                     <Text style={styles.recipeTitleText} numberOfLines={1}>{recipe.title}</Text>
                 </View>
                 <View>
-                    <Image src={serverURL + "/storage/" + recipe.image} style={styles.recipeImage}/>
+                    <Image src={"data:image;base64," + recipe.image} style={styles.recipeImage}/>
                 </View>
             </Pressable>
             <View style={styles.recipeFavoriteAuthorView}>
